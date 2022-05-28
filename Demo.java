0a07bca91f90fd6bbd009d46839ef68f60e3b0ab
Snehal Babar <snehalbabar3012@gmail.com>
2022-05-28 21:12:14 +0530

    Demo.java created

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Que6{
+	public static void main(String args[]){
+		int i=1;
+		while(30>=i){
+			int j=1;
+			while(10>=j){
+				System.out.println(i+" X "+j+" = "+(i*j));
+				j++;
+			}
+			i++;
+		}
+	}
+}
